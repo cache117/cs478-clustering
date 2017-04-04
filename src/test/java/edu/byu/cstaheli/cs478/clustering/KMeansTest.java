872369ceb081926615e99a21f6dc47006ec05b7f
Cache Staheli <cache@byu.edu>
2017-04-04 10:08:06 -0600

    Add abalone data analysis.
    Add spreadsheets detailing analysis.

--- src/test/java/edu/byu/cstaheli/cs478/clustering/KMeansTest.java
+++ src/test/java/edu/byu/cstaheli/cs478/clustering/KMeansTest.java
@@ -31,19 +31,30 @@ class KMeansTest
     {
         String[] args;
         MLSystemManager manager = new MLSystemManager();
-//        assertTrue(new File(datasetsLocation + "abalone/unnormalized.csv").delete());
-//        assertTrue(new File(datasetsLocation + "abalone/normalized.csv").delete());
+        assertTrue(new File(datasetsLocation + "abalone/unnormalized.csv").delete());
+        assertTrue(new File(datasetsLocation + "abalone/normalized.csv").delete());
         for (int k = 2; k < 8; ++k)
         {
             args = ("-L kmeans -A " + datasetsLocation + "abalone.arff -E cluster " + k + " -V").split(" ");
             KMeans kMeans = new KMeans(k, new Random());
             kMeans.setOutputFile(datasetsLocation + "abalone/unnormalized.csv");
+            kMeans.setUseLastColumnOfDataset(true);
             manager.setLearner(kMeans);
             manager.run(args);
 
             args = ("-L kmeans -A " + datasetsLocation + "abalone.arff -E cluster " + k + " -V -N").split(" ");
             kMeans = new KMeans(k, new Random());
             kMeans.setOutputFile(datasetsLocation + "abalone/normalized.csv");
+            kMeans.setUseLastColumnOfDataset(true);
+            manager.setLearner(kMeans);
+            manager.run(args);
+        }
+        for (int k = 2; k < 8; ++k)
+        {
+            args = ("-L kmeans -A " + datasetsLocation + "abalone.arff -E cluster " + k + " -V -N").split(" ");
+            KMeans kMeans = new KMeans(k, new Random());
+            kMeans.setOutputFile(datasetsLocation + "abalone/silhouette.csv");
+            kMeans.setUseLastColumnOfDataset(true);
             manager.setLearner(kMeans);
             manager.run(args);
         }
